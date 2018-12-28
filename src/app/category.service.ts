4537c0c67a84feeb46ddd0137dc6871acdb76421
lexuanduy <lexuanduy07@gmail.com>
2018-12-28 18:26:52 +0700

    create data in categories, get data in option select create course

---
+++ src/app/category.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private db: AngularFirestore) { }
+  getCategories() {
+    return this.db.collection('categories').snapshotChanges().pipe(map(categories => {
+      return categories.map(a => {
+        const value = a.payload.doc.data;
+        const key = a.payload.doc;
+        alert(a.payload.doc.data.name);
+        return {key, ...value};
+      });
+    }));
+  }
+}
--- src/app/courseeditor/courseeditor.component.ts
+++ src/app/courseeditor/courseeditor.component.ts
@@ -3,6 +3,7 @@ import { FormControl } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { CategoryService } from 'src/app/category.service';
 
 @Component({
     selector: 'app-courseeditor',
@@ -10,27 +11,34 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
     styleUrls: ['./courseeditor.component.css']
 })
 export class CourseeditorComponent implements OnInit {
-
-    constructor(db: AngularFirestore) {
+    // categories$;
+    constructor(db: AngularFirestore, public categoryService: CategoryService) {
+        // this.categories$ = categoryService.getCategories();
         this.courseForm = new FormGroup({
+            category: new FormControl(''),
             name: new FormControl(''),
             author: new FormControl(''),
             price: new FormControl(''),
             description: new FormControl(''),
             lecturerId: new FormControl(''),
-            time: new FormControl(''),
+            hours: new FormControl(''),
             hotCourse: new FormControl(''),
+            img: new FormControl('')
         });
+        this.categoryCollection = db.collection('categories');
         this.courseCollection = db.collection('courses', ref => ref.where('name', '==', 'Java'));
         this.partCollection = db.collection('parts', ref => ref.where('courseName', '==', 'Java').where('name', '==', 'Phần 1'));
+        this.categories = this.categoryCollection.valueChanges();
         this.courses = this.courseCollection.valueChanges();
         this.parts = this.partCollection.valueChanges();
         this.lessons = db.collection('parts', ref => ref.where('courseName', '==', 'Java'))
             .doc('JavaPart1').collection('lessons').valueChanges();
     }
+    private categoryCollection: AngularFirestoreCollection<Category>;
     private courseCollection: AngularFirestoreCollection<Course>;
     private partCollection: AngularFirestoreCollection<Part>;
     courseForm: FormGroup;
+    categories: Observable<Category[]>;
     courses: Observable<Course[]>;
     parts: Observable<Part[]>;
     lessons: Observable<any[]>;
@@ -47,14 +55,19 @@ export class CourseeditorComponent implements OnInit {
             });
     }
 }
+export interface Category {
+    name: string;
+}
 export interface Course {
+    category: string;
     name: string;
     author: string;
     price: string;
     description: string;
     lecturerId: string;
-    time: string;
+    hours: string;
     hotCourse: string;
+    img: string;
 }
 export interface Part {
     courseName: string;
